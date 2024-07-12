ebfb215762327a4e64724fb1598d4a0b62cf6e72
John Paxton <johnpaxton@gmail.com>
2024-07-12 14:33:27 -0400

    Initial configuration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,22 +8,4 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'angular-capstone' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-capstone');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-capstone');
-  });
 });
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 
-import { routes } from './app.routes';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {UIRouterModule} from '@uirouter/angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideHttpClient(withFetch()),
+    importProvidersFrom(
+      UIRouterModule.forRoot({ })
+    ),
+  ],
 };
